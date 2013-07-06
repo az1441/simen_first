71bbc52084c51481d674e158f565771297b704f1
Simen Stein <simen.stein@gmail.com>
2013-07-06 11:01:39 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimenFirst::Application.config.secret_token = '8e4cfb82ad1d86633c35a25bba9e1ce06814a9b2e8c7a4fd06a2b3f4edd604e26a62131672e96af9bee9251451272137bf9490190f51eb56ab67141524c34c50'
